ed60e99b878dc1be3b5cd59f10fc4cd895e787f8
dorotajachtoma <jachtoma.do@gmail.com>
2023-09-05 18:11:54 +0200

    create basic entities

--- src/main/java/com/djachtoma/reference/entity/model/Person.java
+++ src/main/java/com/djachtoma/reference/entity/model/Person.java
@@ -1,14 +1,12 @@
 package com.djachtoma.reference.entity.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
